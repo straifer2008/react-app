25d9d09035664a1a325413d6136491189bd1ca14
straifer2008 <a.oktisyuk@gmail.com>
2018-06-22 19:49:30 +0300

    some edit

--- src/config/redux/actions/toggleMenuAction.js
+++
@@ -1,9 +0,0 @@
-// ACTIONS
-import {TOGGLE_MENU} from "../constants";
-
-export function toggleMenuAction() {
-    return {
-        type: TOGGLE_MENU,
-        payload: null
-    };
-}
--- src/config/redux/reducers/routerAnimationTime.js
+++
@@ -1,9 +0,0 @@
-// START STATE
-const initialState = {
-    animationTime: 700
-};
-
-// REDUCERS
-export const routerAnimationTimeReducer = (state = initialState) => {
-    return {...state};
-};
